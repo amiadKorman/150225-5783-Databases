3ab56aafb4cfe020418ec82877daa941ab4019aa
Lior Cohen <53180618+LiorxCohen@users.noreply.github.com>
2023-04-30 22:45:58 +0300

    finish level 4

---
+++ table.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Judge
+(
+  ID INT NOT NULL,
+  Fname VARCHAR(10) NOT NULL,
+  Lname VARCHAR(10) NOT NULL,
+  age INT NOT NULL,
+  gender VARCHAR(10) NOT NULL,
+  nationality VARCHAR(2) NOT NULL,
+  experience INT NOT NULL,
+  matches INT NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE Coach
+(
+  ID INT NOT NULL,
+  Fname VARCHAR(10) NOT NULL,
+  Lname VARCHAR(10) NOT NULL,
+  age INT NOT NULL,
+  gender VARCHAR(10) NOT NULL,
+  nationality VARCHAR(2) NOT NULL,
+  experience INT NOT NULL,
+  matches INT NOT NULL,
+  wins INT NOT NULL,
+  losses INT NOT NULL,
+  awards INT NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE Player
+(
+  ID INT NOT NULL,
+  Fname VARCHAR(10) NOT NULL,
+  Lname VARCHAR(10) NOT NULL,
+  age INT NOT NULL,
+  gender VARCHAR(10) NOT NULL,
+  nationality VARCHAR(2) NOT NULL,
+  experience INT NOT NULL,
+  matches INT NOT NULL,
+  wins INT NOT NULL,
+  losses INT NOT NULL,
+  awards INT NOT NULL,
+  weight INT NOT NULL,
+  height INT NOT NULL,
+  position VARCHAR(2) NOT NULL,
+  number INT NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (ID) REFERENCES Coach(ID)
+);
